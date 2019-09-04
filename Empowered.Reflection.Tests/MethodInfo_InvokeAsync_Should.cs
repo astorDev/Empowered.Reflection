3817365a5db71615e4b16c63a00057b0a08945b4
Astor <astor@Egors-MacBook-Pro.local>
2019-09-04 23:21:49 +0300

    initial commit

---
+++ Empowered.Reflection.Tests/MethodInfo_InvokeAsync_Should.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Empowered.Reflection.Tests
+{
+    [TestClass]
+    public class MethodInfo_InvokeAsync_Should
+    {
+        private Type type = typeof(ExampleClass);
+
+        [TestMethod]
+        public async Task ReturnMethodReturnValue_WhenMethodHasReturnValue()
+        {
+            var method = type.GetMethod("MethodReturningString");
+            var result = await method.InvokeAsync(new ExampleClass());
+            Assert.AreEqual("test", result);
+        }
+
+        [TestMethod]
+        public async Task ReturnNull_WhenRunMethodIsVoid()
+        {
+            var method = type.GetMethod("VoidMethod");
+            var result = await method.InvokeAsync(new ExampleClass());
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public async Task ReturnNull_WhenRunMethodIsAsyncAndReturnsJustTask()
+        {
+            var method = type.GetMethod("AsyncMethodWithoutReturnValue");
+            var result = await method.InvokeAsync(new ExampleClass());
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public async Task ChangesInput_WhenRunMethodIsAsync()
+        {
+            var method = type.GetMethod("AsyncMethodJustChangingInput");
+            var list = new List<string>();
+            await method.InvokeAsync(new ExampleClass(), list);
+            Assert.AreEqual("added in method", list[0]);
+        }
+
+        [TestMethod]
+        public async Task ReturnsAsyncReturnValue_WhenMethodIsAsyncWithReturnValue()
+        {
+            var method = type.GetMethod("AsyncMethodReturningString");
+            var result = await method.InvokeAsync(new ExampleClass());
+            Assert.AreEqual("from async method", result);
+        }
+    }
+
+    public class ExampleClass
+    {
+        public string MethodReturningString()
+        {
+            return "test";
+        }
+
+        public void VoidMethod()
+        {
+
+        }
+
+        public async Task AsyncMethodWithoutReturnValue()
+        {
+        }
+
+        public async Task AsyncMethodJustChangingInput(List<string> list)
+        {
+            list.Add("added in method");
+        }
+
+        public async Task<string> AsyncMethodReturningString()
+        {
+            return "from async method";
+        }
+    }
+}
---
+++ Empowered.Reflection/MethodInfoExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Empowered.Reflection
+{
+    public static class MethodInfoExtension
+    {
+        public async static Task<object> InvokeAsync(this MethodInfo method, object instance, params object[] args)
+        {
+            if (method.IsAsync())
+            {
+                if (method.ReturnType.IsGenericType)
+                {
+                    return (object)await (dynamic)method.Invoke(instance, args);
+                }
+
+                await (Task)method.Invoke(instance, args);
+                return null;
+            }
+
+            return method.Invoke(instance, args);
+        }
+
+        public static bool IsAsync(this MethodInfo method)
+        {
+            return method.ReturnType.GetMethod(nameof(Task.GetAwaiter)) != null;
+        }
+    }
+}
