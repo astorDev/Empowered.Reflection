ef395a8a9546804fe5a2c529197e5a221c088c4a
Егор <Pavluchenk0@yandex.ru>
2019-09-09 12:16:01 +0300

    Добавил возможность поиска метода

---
+++ Empowered.Reflection.Tests/ExampleClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empowered.Reflection.Tests
+{
+    public class ExampleClass
+    {
+        public string MethodReturningString()
+        {
+            return "test";
+        }
+
+        public void VoidMethod()
+        {
+
+        }
+
+        public async Task AsyncMethodWithoutReturnValue()
+        {
+        }
+
+        public async Task AsyncMethodJustChangingInput(List<string> list)
+        {
+            list.Add("added in method");
+        }
+
+        public async Task<string> AsyncMethodReturningString()
+        {
+            return "from async method";
+        }
+    }
+}
